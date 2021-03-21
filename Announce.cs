a3eb25df17c969766ea32375a0860779b68e44da
Marcos Mont <78879585+404mqs@users.noreply.github.com>
2021-03-21 20:57:02 -0300

    Add files via upload
    Optimized announce command.

---
+++ Announce.cs
@@ -0,0 +1,32 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Announcer
+{
+
+    public class Announce : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+        public string Name => "announce";
+        public string Help => "";
+        public string Syntax => throw new NotImplementedException();
+        public List<string> Aliases => new List<string> { "anunciar" };
+        public List<string> Permissions => new List<string> { "an.announce" };
+
+        public void Execute(IRocketPlayer caller, string[] args)
+        {
+            var name = caller.DisplayName;
+            var message = string.Join(" ", args);
+            var title = MQSPlugin.Instance.Configuration.Instance.MessageTitle;
+            EffectManager.sendUIEffect(12800, 4205, true, title, message, name);
+        }
+
+    }
+
+}
